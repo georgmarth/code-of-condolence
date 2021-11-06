88c2c72ac201514deea3cc2f2acec7f8d798c9bb
Georg Marth <georg.marth@kolibrigames.com>
2021-11-06 13:26:17 +0100

    add CharacterController and basic movement

---
+++ Assets/Scripts/ClickEventManager.cs
@@ -0,0 +1,6 @@
+﻿using UniRx;
+
+public class ClickEventManager : Singleton<ClickEventManager>
+{
+    public readonly Subject<ClickEvent> Clicks = new Subject<ClickEvent>();
+}
---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    public Collider2D Collider;
+    public readonly Subject<Unit> Clicked = new Subject<Unit>();
+    public Interactable Interactable;
+
+    private void Start()
+    {
+        Collider.OnMouseUpAsButtonAsObservable()
+            .Subscribe(_ => OnClick());
+    }
+
+    private void OnClick()
+    {
+        ClickEventManager.Instance.Clicks.OnNext(new ClickEvent { ClickTarget = Interactable });
+        Clicked.OnNext(Unit.Default);
+    }
+}
