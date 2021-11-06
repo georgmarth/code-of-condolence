1c3858892b862471bd59ac6fbbd33173043f9ec9
Georg Marth <georg.marth@kolibrigames.com>
2021-11-06 15:43:37 +0100

    add camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float MinPosition = -10;
+    public float MaxPosition = 10;
+
+    public float SmoothTime = 3;
+
+    private float _currentVelocity;
+    
+    private void LateUpdate()
+    {
+        var targetXPosition = Mathf.Clamp(PlayerController.Instance.transform.position.x, MinPosition, MaxPosition);
+
+        var position = transform.position;
+        var smoothTarget = Mathf.SmoothDamp(position.x, targetXPosition, ref _currentVelocity, SmoothTime);
+
+        position = new Vector3(smoothTarget, position.y, position.z);
+        transform.position = position;
+    }
+}
