97ec05a3409543b9b3f9751e18afec0568d63a4f
Georg Marth <georg.marth@kolibrigames.com>
2021-11-07 14:13:40 +0100

    add Letter to game menu

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -7,12 +7,20 @@ public class GameMenu : MonoBehaviour
     public Button StartButton;
     public Button ExitButton;
     public Canvas Canvas;
+    public GameObject Letter;
+    public AudioSource Narrator;
 
+    private bool _letterWasShown;
+    
     private void Start()
     {
+        SetStartMenuActive(true);
+        _letterWasShown = false;
+        Letter.gameObject.SetActive(false);
+
         StartButton
             .OnClickAsObservable()
-            .Subscribe(_ => SetStartMenuActive(false));
+            .Subscribe(_ => StartGame());
 
         ExitButton.OnClickAsObservable()
             .Subscribe(_ => Application.Quit());
@@ -22,6 +30,31 @@ public class GameMenu : MonoBehaviour
             .DistinctUntilChanged()
             .Where(keyPressed => keyPressed)
             .Subscribe(_ => SetStartMenuActive(true));
+    }
+
+    private void StartGame()
+    {
+        if (!_letterWasShown)
+        {
+            Letter.SetActive(true);
+            Narrator.Play();
+            Observable.EveryUpdate()
+                .Select(_ => Narrator.isPlaying)
+                .Skip(1)
+                .DistinctUntilChanged()
+                .Where(isPlaying => !isPlaying)
+                .Take(1)
+                .Subscribe(_ =>
+                {
+                    _letterWasShown = true;
+                    Letter.SetActive(false);
+                    SetStartMenuActive(false);
+                });
+        }
+        else
+        {
+            SetStartMenuActive(false);
+        }
     }
 
     private void SetStartMenuActive(bool value)
