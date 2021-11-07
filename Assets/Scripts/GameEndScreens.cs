421a6a13401da02f05043b4b0f3525ce3f2a6bb4
Georg Marth <georg.marth@kolibrigames.com>
2021-11-07 16:43:32 +0100

    add Game End Screens with win audio

---
+++ Assets/Scripts/GameEndScreens.cs
@@ -0,0 +1,39 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class GameEndScreens : MonoBehaviour
+{
+    public Canvas GameWinScreen;
+    public Canvas GameOverScreen;
+
+    public AudioClip GameWinAudio;
+    public AudioClip GameOverAudio;
+    
+    private void Start()
+    {
+        GameWinScreen.gameObject.SetActive(false);
+        GameOverScreen.gameObject.SetActive(false);
+        
+        GameStateManager.Instance.GameIsWon
+            .Where(isWon => isWon)
+            .Take(1)
+            .Subscribe(_ => ActivateGameWinScreen());
+
+        GameStateManager.Instance.AllOptionsAreExhausted
+            .Where(isOver => isOver)
+            .Take(1)
+            .Subscribe(_ => ActivateGameOverScreen());
+    }
+
+    private void ActivateGameWinScreen()
+    {
+        GameWinScreen.gameObject.SetActive(true);
+        Music.Instance.SwitchMusic(GameWinAudio);
+    }
+
+    private void ActivateGameOverScreen()
+    {
+        GameOverScreen.gameObject.SetActive(true);
+        Music.Instance.SwitchMusic(GameOverAudio);
+    }
+}
