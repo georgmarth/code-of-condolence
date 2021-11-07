a73a4f859b584f03d010c8a22514d67955228d5c
Georg Marth <georg.marth@kolibrigames.com>
2021-11-07 17:07:41 +0100

    add head wiggle to characters

---
+++ Assets/Scripts/HeadWiggle.cs
@@ -0,0 +1,13 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class HeadWiggle : MonoBehaviour
+{
+    public float RotationDuration = 1f;
+    public float RotationStrength = 20f;
+    
+    private void Start()
+    {
+        transform.DOShakeRotation(RotationDuration, RotationStrength).SetLoops(-1, LoopType.Yoyo);
+    }
+}
